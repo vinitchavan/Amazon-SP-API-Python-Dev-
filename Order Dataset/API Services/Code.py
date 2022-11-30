6b747d1cbfbad0f45fde1715cca2c882e3cda368
Vinit $ Chavan <70135833+vinitchavan@users.noreply.github.com>
2022-11-30 10:50:22 +0530

    Update and rename Code to Code.py

---
+++ Order Dataset/API Services/Code.py
@@ -0,0 +1,8 @@
+
+#for refrence : https://developer-docs.amazon.com/sp-api/docs/orders-api-v0-reference 
+
+#First we need to install python amazon sp api package 
+
+
+!pip install python-amazon-sp-api
+
